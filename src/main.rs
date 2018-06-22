a73ad73ff5b289afb2724f52a93e2b0186a9a2e2
Musee Ullah <lae@lae.is>
2018-06-22 05:30:46 -0700

    Add clap, default to non-percent-encoded URLs and fix pipefail issue

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+#[macro_use]
+extern crate clap;
 extern crate reqwest;
 extern crate scraper;
 extern crate url;
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate try_print;
 
 use scraper::{Html, Selector};
 use url::Url;
+use url::percent_encoding::percent_decode;
 
 error_chain! {
     foreign_links {
         ReqError(reqwest::Error);
         UrlParseError(url::ParseError);
+        Utf8ParseError(std::string::FromUtf8Error);
     }
 }
 
@@ -18,10 +24,35 @@ const HOST: &'static str = "http://sanabutton.ojaru.jp";
 const EXT: &'static str = "mp3";
 
 fn run() -> Result<()> {
+    let matches = clap_app!(magicalstick =>
+                            (version: "0.0.2")
+                            (about: "Show sana button resource list.")
+                            (@arg percents: -u --urlencode "Prints out percent-encoded URLs")
+                           ).get_matches();
+    let mut urls: Vec<String> = Vec::new();
     let sounds_selector = Selector::parse(r#".sounds"#).unwrap();
     for sound in Html::parse_document(&reqwest::get(HOST)?.text()?).select(&sounds_selector) {
-        let url = Url::parse(&format!("{}/{}.{}", HOST, sound.value().attr("data-file").unwrap_or(""), EXT))?;
-        println!("{}", url.as_str());
+        urls.push(
+            Url::parse(
+                &format!("{}/{}.{}", HOST, sound.value().attr("data-file").unwrap_or(""), EXT)
+            )?
+            .into_string()
+        );
+    }
+    if matches.is_present("percents") {
+        if let Err(_) = try_println!("{}", urls.join("\n")) {
+            std::process::exit(0);
+        }
+    } else {
+        let mut pretty_urls: Vec<String> = Vec::new();
+        for url in urls {
+            pretty_urls.push(
+                String::from_utf8(percent_decode(url.into_bytes().as_slice()).collect())?
+            );
+        }
+        if let Err(_) = try_println!("{}", pretty_urls.join("\n")) {
+            std::process::exit(0);
+        }
     }
     Ok(())
 }
